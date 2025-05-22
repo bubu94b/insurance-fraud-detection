7cd443d183d496be84f31e76092e5b2e312b3950
bubu94b <b.buldu@hotmail.fr>
2025-05-22 12:32:40 +0200

    Added ingest.py and clean.py for EDA notebook starting

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,1 +1,49 @@
- 
+import pandas as pd
+import numpy as np
+
+def convert_to_datetime(df):
+    cols_to_convert = [col for col in df.columns if 'date' in col.lower()]
+    if cols_to_convert:
+        for col in cols_to_convert:
+            try:
+                df[col] = pd.to_datetime(df[col], errors='coerce')
+            except Exception as e:
+                print(f'Error during the conversion of the column {col}')
+    else:
+        print('No date columns detected')
+
+def clean_data(df):
+    df = df.copy()
+
+    df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
+
+    convert_to_datetime(df)
+
+    numeric_cols = ['policy_annual_premium', 'total_claim_amount', 'injury_claim', 'property_claim', 'vehicle_claim']
+    for num_col in numeric_cols:
+        if num_col in df.columns:
+            df[num_col] = df[num_col].astype(str).str.replace(',', '.', regex=False)
+            df[num_col] = pd.to_numeric(df[num_col], errors='coerce')
+
+    df['police_report_available'] = df['police_report_available'].str.replace('?', 'UNKNOWN')
+
+    dummies = pd.get_dummies(df['police_report_available'], prefix='police_report_available')
+    df = pd.concat([df, dummies], axis=1)
+
+    df['police_report_available'] = df['police_report_available'].map({
+        'YES': 1,
+        'NO': 0,
+        'UNKNOWN': -1
+    })
+
+    df['fraud_reported'] = df['fraud_reported'].apply(lambda x: x.upper()).map({
+        'Y': 1,
+        'N': 0
+    })
+
+    if 'auto_year' in df.columns:
+        df['auto_year'] = pd.to_numeric(df['auto_year'], errors='coerce')
+
+    df.drop_duplicates(inplace=True)
+
+    return df
--- scripts/ingest.py
+++ scripts/ingest.py
@@ -1,1 +1,13 @@
- 
+from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient
+import pandas as pd
+import os
+from io import StringIO
+
+def load_data():
+    load_dotenv()
+    conn_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    blob_client = BlobServiceClient.from_connection_string(conn_str).get_blob_client(
+        container="raw-data", blob="claims_data.csv")
+    csv_data = blob_client.download_blob().readall().decode("utf-8")
+    return pd.read_csv(StringIO(csv_data), sep=';')
